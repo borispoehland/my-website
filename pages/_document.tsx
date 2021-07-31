ed1c47f872d27d0275ea2e4fb4ce0a3fdf874ecc
Boris Pöhland <boris.poehland.business@gmail.com>
2021-07-31 11:49:16 +0200

    lighthouse optimizations (#3)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,10 +3,10 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
-          {/*eslint-disable-next-line @next/next/no-sync-scripts*/}
-          <script src="/scripts/jquery-3.5.1.slim.min.js" />
+          {/* TODO: next-seo, favicons */}
+          <script defer src="/scripts/jquery-3.5.1.slim.min.js" />
         </Head>
         <body>
           <Main />
