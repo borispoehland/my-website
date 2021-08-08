5809d7e4efc7fcec3c1c75898ddfa53b208f12d9
Boris Pöhland <boris.poehland.business@gmail.com>
2021-08-08 14:06:45 +0200

    changed the mobile nav. Still not 100% satisfied, thinking about a full-page slide-in nav (#8)

--- src/components/Navbar/subcomponents/NavToggler.tsx
+++ src/components/Navbar/subcomponents/NavToggler.tsx
@@ -1,19 +1,13 @@
 import { MouseEventHandler } from 'react';
 import { Navbar } from 'react-bootstrap';
-import cx from 'classnames';
 
 interface IProps {
-  isOpen: boolean;
   onClick: MouseEventHandler;
 }
 
-const NavToggler = ({ isOpen, onClick }: IProps): JSX.Element => (
+const NavToggler = ({ onClick }: IProps): JSX.Element => (
   <Navbar.Toggle onClick={onClick}>
-    <div
-      className={cx('navbar-toggler__icon', {
-        '--is-open': isOpen,
-      })}
-    >
+    <div className="navbar-toggler__icon">
       <span />
       <span />
       <span />
