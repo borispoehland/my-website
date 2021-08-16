5549dfb8a4a19582b55bc897bc1eb0d784740b2a
Boris Pöhland <boris.poehland@gmail.com>
2021-08-16 13:39:19 +0200

    title tweak in CV + refactored the form submission process

--- src/utils/dom.ts
+++ src/utils/dom.ts
@@ -9,13 +9,20 @@ export const openResourceInNewTab = (path: string): void => {
   window.open(path, '__blank');
 };
 
-export const fireSweetAlert = (
-  type: 'success' | 'error',
-  message: string
-): Promise<void> => {
+export interface ISwalProps {
+  swalTitle: string;
+  swalDescription: string;
+  swalType: 'success' | 'error';
+}
+
+export const fireSweetAlert = ({
+  swalDescription,
+  swalTitle,
+  swalType,
+}: ISwalProps): Promise<void> => {
   return Swal.fire({
-    title: type === 'success' ? 'Success!' : 'Something went wrong',
-    icon: type,
-    html: message,
+    title: swalTitle,
+    icon: swalType,
+    html: swalDescription,
   });
 };
