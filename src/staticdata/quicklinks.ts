6c4ac6939ed6d3562baa38db69b3a8fe4a870efa
Boris Pöhland <boris.poehland@gmail.com>
2021-08-16 10:26:38 +0200

    contact page: put cv above quicklinks, added new quicklinks, made quicklinks column on sm

--- src/staticdata/quicklinks.ts
+++ src/staticdata/quicklinks.ts
@@ -1,5 +1,6 @@
-import { FiMail, FiTwitter } from 'react-icons/fi';
+import { FiInstagram, FiMail, FiTwitter } from 'react-icons/fi';
 import { IQuicklink } from '@components/QuicklinkBar/converters/ToQuicklinkConverter';
+import { FaLinkedin } from 'react-icons/fa';
 
 const getQuicklinks = (): IQuicklink[] => [
   {
@@ -8,9 +9,19 @@ const getQuicklinks = (): IQuicklink[] => [
     Icon: FiMail,
   },
   {
+    label: 'Boris Pöhland',
+    href: 'https://www.linkedin.com/in/boris-p%C3%B6hland-541a28207/',
+    Icon: FaLinkedin,
+  },
+  {
     label: '@borispoehland',
     href: 'https://twitter.com/borispoehland',
     Icon: FiTwitter,
+  },
+  {
+    label: '@borispoehland',
+    href: 'https://www.instagram.com/borispoehland/',
+    Icon: FiInstagram,
   },
 ];
 
