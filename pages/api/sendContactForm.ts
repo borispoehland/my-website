4328151badee280fc3e7214a7ff717176dcdcef2
Boris Pöhland <boris.poehland@gmail.com>
2021-08-16 10:55:59 +0200

    added ratelimiter to contact form

--- pages/api/sendContactForm.ts
+++ pages/api/sendContactForm.ts
@@ -1,17 +1,31 @@
+import { RateLimiterMemory } from 'rate-limiter-flexible';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import mailer from '@sendgrid/mail';
 import getMails from '../../src/staticdata/mails';
 
 mailer.setApiKey(process.env.SENDGRID_API_KEY);
 
+const rateLimiter = new RateLimiterMemory({
+  points: 2, // 2 inquiries
+  duration: 60 * 60, // per hour
+});
+
 export interface ISendMailResponse {
   myMail?: string;
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ISendMailResponse>
 ) {
+  const clientIP = req.headers['x-real-ip'] || req.socket.remoteAddress || '';
+
+  try {
+    await rateLimiter.consume(Array.isArray(clientIP) ? clientIP[0] : clientIP);
+  } catch (err) {
+    res.status(429).send({});
+  }
+
   return mailer
     .send(getMails(req.body))
     .then(() => {
