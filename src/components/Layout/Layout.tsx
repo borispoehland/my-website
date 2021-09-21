4485b688b3056d24472cfa374e002a4c57611feb
Boris Pöhland <boris.poehland.business@gmail.com>
2021-09-21 14:55:44 +0200

    added scroll to top button (#30)
    * added scroll to top button
    
    * margin-right between md and max-page-width
    
    * final changes

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
 import Footer from '../Footer/Footer';
 import Image from 'next/image';
 import Navbar from '@components/Navbar/Navbar';
+import ScrollToTopButton from '@components/ScrollToTopButton/ScrollToTopButton';
 
 interface IProps extends HasChildren {}
 
@@ -12,6 +13,9 @@ const Layout = ({ children }: IProps): JSX.Element => {
       <Navbar className="page__navbar" />
       <Breadcrumbs className="page__breadcrumbs" />
       <div className="page__content">{children}</div>
+      <div className="page__scroll-up">
+        <ScrollToTopButton />
+      </div>
       <Footer className="page__footer" />
       <div className="page__bottom-waves">
         <Image src="/img/wave.svg" alt="" layout="fill" objectFit="cover" />
---
+++ src/components/ScrollToTopButton/ScrollToTopButton.tsx
@@ -0,0 +1,29 @@
+import Button from '@components/Button/Button';
+import { HiArrowUp } from 'react-icons/hi';
+import { useHideScrollToTopButtonWhenOnTop } from '@components/ScrollToTopButton/hooks';
+import { reAppendDOMNode } from '@utils/dom';
+
+const ScrollToTopButton = (): JSX.Element => {
+  useHideScrollToTopButtonWhenOnTop();
+
+  return (
+    <Button
+      onClick={({ target }) => {
+        $('html, body').animate(
+          { scrollTop: 0 },
+          {
+            duration: 'slow',
+            complete() {
+              reAppendDOMNode(target as HTMLElement);
+            },
+          }
+        );
+      }}
+      className="to-top-button"
+    >
+      <HiArrowUp />
+    </Button>
+  );
+};
+
+export default ScrollToTopButton;
